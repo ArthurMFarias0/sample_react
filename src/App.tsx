11600f5819d26a61eb8b1d8dbf5c6d17159e712c
Arthur <arthuremeunome@gmail.com>
2022-11-25 12:05:26 -0300

    feat: configurando as rotas do sistema

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css';
 import React from "react";
-import { Title } from './components/Title/Title';
-import { Text } from './components/Text/Text';
-import { Nav } from './components/Nav/Nav';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/estaticos/navbar/Navbar';
+import { Home } from './components/paginas/home/Home';
+import { About } from './components/paginas/about/About';
 
 function App() {
   const nome = 'Arthur';
   return (
     <>
-      <Nav/>
-      <Title nome="Arthur"/>
-      <Text />
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </Router>
     </>
   );
 }
